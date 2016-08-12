7d198b4cf12857b2d9160dce60efe85a1acca369
Patrick White <pfwhite@ufl.edu>
2016-08-12 14:55:45 -0400

    facets and project tag

---
+++ qipr/registry/forms/facet_form.py
@@ -0,0 +1,21 @@
+from registry.models import *
+from operator import attrgetter
+
+facet_Models = [
+    BigAim,
+    Category,
+    ClinicalArea,
+    ClinicalSetting,
+    ClinicalDepartment,
+    Keyword,
+    SafetyTarget,
+]
+
+class FacetForm:
+    def __init__(self):
+        self.facet_categories = [model.__name__ for model in facet_Models]
+        for model in facet_Models:
+            models = list(model.objects.all())
+            models.sort(key=lambda m : m.project_set.count(), reverse=True)
+            setattr(self, model.__name__, models)
+
--- qipr/registry/templatetags/project_tag.py
+++ qipr/registry/templatetags/project_tag.py
@@ -8,7 +8,19 @@ def project_tag(project):
     Renders a project
     takes a project
     """
+    fields = project['fields']
     context = {
         'project': str(project),
+        'project_id': project['pk'],
+        'title': fields['title'],
+        'description': fields['description'],
+        'owner_string': __format_owner_string(fields['owner']),
+        'owner_id': __get_owner_id(fields['owner']),
     }
     return context
+
+def __format_owner_string(owner_natural_key):
+    return ' '.join([str(item) for item in owner_natural_key[2:]])
+
+def __get_owner_id(owner_natural_key):
+    return owner_natural_key[0]
