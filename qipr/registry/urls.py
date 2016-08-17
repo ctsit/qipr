7c32ee07cd13b8fcf5745f573fba2ac6763879af
ndavuluri <samanthadavuluri4444@gmail.com>
2016-08-17 18:57:41 -0400

    Adding one project to fixture and Adding styles to Project_Info Page

--- qipr/registry/urls.py
+++ qipr/registry/urls.py
@@ -4,5 +4,5 @@ from registry import api
 
 urlpatterns = [
     url(r'^(?:d=(?P<descriptors_json>\[.*\])/)?$', views.index, name='index'),
-    url(r'^project_info$',views.project_info, name='project_info'),
+    url(r'^project_info/(?P<project_id>[0-9]+)/$',views.project_info, name='project_info'),
 ]
