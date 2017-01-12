0b8e11dc8410db74142e03173442ebf8a3404e4e
Patrick White <pfwhite@ufl.edu>
2017-01-12 11:09:30 -0500

    its working! checking related models

--- qipr/registry/api/add_model.py
+++ qipr/registry/api/add_model.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
-from registry.models import User
+# from registry.models import User
 from registry.workflows import api_workflow
 from registry.constants import bridge_keys, approver_username
 
@@ -9,25 +10,25 @@ import json
 import hashlib
 
 @csrf_exempt
-def add_model(request):
+def add_model(request, req_hash):
     if request.method == 'POST':
         data = request.body.decode('utf-8')
-        user = __get_user_from_hash(data)
+        user = __get_user_from_hash(data, req_hash)
         if user:
             res_data = api_workflow.translate_and_add_model(user, data)
         return JsonResponse({'body': res_data}, safe=False) if user else HttpResponseForbidden()
     else:
-        return HttpResponseForbidden()
+        # return HttpResponseForbidden()
+        return JsonResponse({'body': req_hash}, safe=False)
 
 
-def __get_user_from_hash(json_data):
+def __get_user_from_hash(json_data, req_hash):
 # try:
-    data = json.loads(json_data)
-    req_hash = data['hash']
-    del data['hash']
-    json_data = json.dumps(data)
+    # data = json.loads(json_data)
+    # req_hash = data['hash']
+    # json_data = json.dumps(data)
     to_hash = json_data.encode('utf-8') + bridge_keys['approver'].encode('utf-8')
-    check_hash = hashlib.md5(to_hash).hex_digest()
+    check_hash = hashlib.md5(to_hash).hexdigest()
     if req_hash == check_hash:
         return User.objects.get(username=approver_username)
 # except:
--- qipr/registry/urls.py
+++ qipr/registry/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^search/(?:s=(?P<search_text>[\w\s]*)/)?(?:d=(?P<descriptors_json>\[.*\])/)?$', views.search, name='search'),
     url(r'^project_info/(?P<project_id>[0-9]+)/$',views.project_info, name='project_info'),
     url(r'^$',views.index, name='index'),
-    url(r'^api/add_model$', api.add_model, name='add_model'),
+    url(r'^api/add_model/(?P<req_hash>[0-9a-fA-F]+)$', api.add_model, name='add_model'),
 ]
