ae0c4dd2e25158672be9781260770732d3e22ce1
Patrick White <pfwhite@ufl.edu>
2017-01-19 11:41:15 -0500

    temporarily disable audittrail

--- qipr/registry/signals/__init__.py
+++ qipr/registry/signals/__init__.py
@@ -1,4 +1,4 @@
 from registry.signals.model_lists import AllNormalModels
 from registry.signals import all_signals
 
-from registry.signals import audit_trail_signals
+# from registry.signals import audit_trail_signals
--- qipr/registry/signals/all_signals.py
+++ qipr/registry/signals/all_signals.py
@@ -1,3 +1,3 @@
 from registry.signals import audit_trail_signals
 
-audit_trail_signals.connect_signals()
+# audit_trail_signals.connect_signals()
