ce414d1ac52c48d9dde29a23cfa353d4e65ff80d
Patrick White <pfwhite@ufl.edu>
2017-01-24 13:48:59 -0500

    Fixed display of facet titles

--- qipr/registry/forms/facet_form.py
+++ qipr/registry/forms/facet_form.py
@@ -8,6 +8,7 @@ related_by_projects_Models = [
     Descriptor,
 ]
 
+
 class FacetForm:
     def __init__(self):
         self.facet_categories = [model.__name__ for model in related_by_projects_Models]
@@ -15,4 +16,13 @@ class FacetForm:
             models = list(model.objects.all())
             models.sort(key=lambda m : m.projects.count(), reverse=True)
             setattr(self, model.__name__, models)
+
+    def get_display(self, facet_category):
+        displays = {
+            'BigAim': 'Big Aim',
+            'ClinicalArea': 'Clinical Area',
+            'ClinicalSetting': 'Clinical Setting',
+            'Descriptor': 'MeSH Keyword',
+        }
+        return displays[facet_category]
 
